d80d78df0c832f2191c1fd9b6c06c4a86f876e51
Henilkaneriya13 <kaneriyahenil@gmail.com>
2022-11-24 12:12:16 +0530

    p

---
+++ c programming/class work/mydata.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{ 
+
+
+     printf("first name : henil\n");
+     printf("last name  : kaneriya\n");
+     printf("email : kaneriyahenil@gmail.com\n");
+     printf("mobile mo. : 6353810999");
+
+}
