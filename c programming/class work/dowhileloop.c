2e5165f9315cc9ce8621734c8cbb8ed379aa4286
Henilkaneriya13 <kaneriyahenil@gmail.com>
2022-12-06 11:52:52 +0530

    p

---
+++ c programming/class work/dowhileloop.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/*initialization 
+
+do
+{  
+        statement;
+		updation;
+		
+}
+while(condition);
+*/
+
+
+void main()
+{
+	
+	int i = 1;
+	
+	do
+	{ 
+		
+		printf("%d",i);
+		i++;
+	}
+	while(i<=1000);
+	
+	printf("%d\n",i);
+	i++;
+	
+}
+
---
+++ c programming/class work/whileloop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+         
+         
+         //syntax :  initialization;
+         
+         //while(condition)
+         //{ 
+         //statement;
+         //updation;
+		 //}
+         
+              
+void main()
+{
+	int i = 1;
+	
+	while(i<10)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+}
