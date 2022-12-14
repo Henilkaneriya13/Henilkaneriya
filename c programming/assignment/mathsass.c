1d35e7b1dcc416e10bf59f14665b18ff87c92468
Henilkaneriya13 <kaneriyahenil@gmail.com>
2022-12-14 18:15:13 +0530

    assignment-2 program

---
+++ c programming/assignment/mathsass.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+	
+	int n1 =34;
+	int n2 =23;
+	
+printf("\n\xB2\xB2\xB2\xB2 calculator \xB2\xB2\xB2\xB2\xB2\xB2");
+//	printf("a=%d\nb=%d",n1,n2);
+	  
+	  
+	printf("\naddition : %d",(n1+n2));
+	printf("\nsubstraction : %d",(n1-n2));
+	printf("\nmultiplication : %d",(n1*n2));
+    printf("\ndivision : %.2f",((float)n1/n2));
+	
+	
+		
+}
